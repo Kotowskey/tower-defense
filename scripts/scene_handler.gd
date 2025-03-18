4da20d82d677ed626ad72ca14d82ec423748506a
Łukasz Kotowski <lukaszkotowski@protonmail.com>
2025-03-18 13:36:57 +0100

    scene_handler + game_scene

--- scripts/scene_handler.gd
+++ scripts/scene_handler.gd
@@ -5,7 +5,9 @@ func _ready():
 	get_node("Menu/MarginContainer/VBoxContainer/QUIT").connect("pressed", Callable(self, "on_quit_pressed"))
 	
 func on_new_game_pressed():
-	pass
+	get_node("Menu").queue_free()
+	var game_scene = load("res://scenes/game_scene.tscn").instantiate()
+	add_child(game_scene)
 
 func on_quit_pressed():
 	get_tree().quit()
