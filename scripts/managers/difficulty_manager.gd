a22fccbf9a4b96d29f775a7e962787422c575ae0
Łukasz Kotowski <lukaszkotowski@protonmail.com>
2025-10-17 13:38:40 +0200

    difficulty fix

--- scripts/managers/difficulty_manager.gd
+++ scripts/managers/difficulty_manager.gd
@@ -6,22 +6,22 @@ var current_difficulty = Difficulty.NORMAL
 
 var difficulty_multipliers = {
 	Difficulty.EASY: {
-		"enemy_health": 0.7,
-		"enemy_speed": 0.8,
-		"player_money": 1.5,
-		"enemy_reward": 1.2
-	},
-	Difficulty.NORMAL: {
 		"enemy_health": 1.0,
 		"enemy_speed": 1.0,
 		"player_money": 1.0,
 		"enemy_reward": 1.0
 	},
+	Difficulty.NORMAL: {
+		"enemy_health": 1.1,
+		"enemy_speed": 1.,
+		"player_money": 1.0,
+		"enemy_reward": 1.0
+	},
 	Difficulty.HARD: {
-		"enemy_health": 1.5,
+		"enemy_health": 1.2,
 		"enemy_speed": 1.2,
-		"player_money": 0.7,
-		"enemy_reward": 0.8
+		"player_money": 1.0,
+		"enemy_reward": 1.0
 	}
 }
 
