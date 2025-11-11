70dab9f8cc465916fb1b376d3020aed9fed9c97b
Łukasz Kotowski <lukaszkotowski@protonmail.com>
2025-11-11 19:03:12 +0100

    campaign mode

--- scripts/ui/ui_manager.gd
+++ scripts/ui/ui_manager.gd
@@ -346,25 +346,19 @@ func update_campaign_info():
 	var level_number = mode_manager.get_campaign_level()
 	var level = CampaignLevel.get_level(level_number)
 	
-	if level and game_scene.has_node("UI/HUD/InfoPanel/UserUI/WaveContainer"):
-		var wave_container = game_scene.get_node("UI/HUD/InfoPanel/UserUI/WaveContainer")
+	if level and game_scene.has_node("UI/HUD/InfoPanel"):
+		var info_panel = game_scene.get_node("UI/HUD/InfoPanel")
+		var info_title = game_scene.get_node("UI/HUD/InfoPanel/InfoTitle")
 		
-		if not wave_container.has_node("LevelNameLabel"):
+		if not info_panel.has_node("LevelNameLabel"):
 			var level_name_label = Label.new()
 			level_name_label.name = "LevelNameLabel"
 			level_name_label.text = level.level_name
-			level_name_label.add_theme_font_size_override("font_size", 20)
+			level_name_label.add_theme_font_size_override("font_size", 16)
 			level_name_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-			wave_container.add_child(level_name_label)
-			wave_container.move_child(level_name_label, 0)
-		
-		if not wave_container.has_node("LevelDescLabel") and level.level_description != "":
-			var level_desc_label = Label.new()
-			level_desc_label.name = "LevelDescLabel"
-			level_desc_label.text = level.level_description
-			level_desc_label.add_theme_font_size_override("font_size", 14)
-			level_desc_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-			level_desc_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-			level_desc_label.custom_minimum_size = Vector2(250, 0)
-			wave_container.add_child(level_desc_label)
-			wave_container.move_child(level_desc_label, 1)
+			info_panel.add_child(level_name_label)
+			level_name_label.set_anchors_preset(Control.PRESET_TOP_WIDE)
+			level_name_label.offset_top = 22
+			level_name_label.offset_bottom = 38
+			
+			info_title.offset_bottom = 22
