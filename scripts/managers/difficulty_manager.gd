20af7f3daddeaaa0245b7715f4307b4dd4ad1424
Łukasz Kotowski <lukaszkotowski@protonmail.com>
2026-01-03 14:44:25 +0100

    Update difficulty_manager.gd

--- scripts/managers/difficulty_manager.gd
+++ scripts/managers/difficulty_manager.gd
@@ -12,14 +12,14 @@ var difficulty_multipliers = {
 		"enemy_reward": 1.0
 	},
 	Difficulty.NORMAL: {
-		"enemy_health": 1.25,
-		"enemy_speed": 1.25,
+		"enemy_health": 1.5,
+		"enemy_speed": 1.5,
 		"player_money": 0.8,
 		"enemy_reward": 0.8
 	},
 	Difficulty.HARD: {
-		"enemy_health": 1.5,
-		"enemy_speed": 1.5,
+		"enemy_health": 2.0,
+		"enemy_speed": 2.0,
 		"player_money": 0.6,
 		"enemy_reward": 0.6
 	}
